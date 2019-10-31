7440f761a897390676ece64c7681d16cd1d1dd4f
Sergej Miroshnikov <SVM1983@tut.by>
2019-10-31 22:02:37 +0200

    portfolio:feature: implement slider

--- js/code.js
+++ js/code.js
@@ -29,6 +29,32 @@ let projects_switch = document.getElementById("projects_switch");
 projects_switch.addEventListener("click", changeProjectsState, false);
 
 let projects_slider = document.getElementById("projects_slider");
+// projects_slider.addEventListener("click", switchProjectsSliderImage, false);
+
+let project_slider_chevron_left = document.getElementById("project_slider_chevron_left");
+let project_slider_chevron_right = document.getElementById("project_slider_chevron_right");
+project_slider_chevron_left.addEventListener("click", switchProjectsSliderImage, false);
+project_slider_chevron_right.addEventListener("click", switchProjectsSliderImage, false);
+
+let data = ["img/Image_screen_project_1.png","img/Image_screen_project_2.png"];
+let data_index = 0;
+
+function switchProjectsSliderImage(){
+	if(event.currentTarget.id === "project_slider_chevron_left"){
+		if(data_index - 1 >= 0){image_screen_project.src = data[--data_index];}
+		else{
+			data_index = data.length - 1;
+			image_screen_project.src = data[data_index];
+		}
+	}
+	else if(event.currentTarget.id === "project_slider_chevron_right"){
+		if(data_index + 1 > data.length - 1){
+			data_index = 0;
+			image_screen_project.src = data[data_index];
+		}
+		else{image_screen_project.src = data[++data_index];}
+	}
+}
 
 function changeProjectsState(){
 	if(projects_switch.className === "fa fa-chevron-down  fa-fw"){
@@ -48,3 +74,15 @@ function showProjectsInfo(){
 function hideProjectsInfo(){
 	projects_slider.className = "projects-content-disable";
 }
+
+let image_screen_project = document.getElementById("image_screen_project");
+
+
+
+
+
+
+
+
+
+
